74276d22a165b492d62be87a5896e8d0f70ef451
Naoga <naoga.taka@gmail.com>
2016-04-24 16:16:56 +0900

    メソッドの追加テスト

--- app/src/main/java/com/study/endeza/MainActivity.java
+++ app/src/main/java/com/study/endeza/MainActivity.java
@@ -11,7 +11,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = (TextView)findViewById(R.id.text);
-        textView.setText("こんにちは");
-        //aaa
+        String s = calc(100);
+        s += calc(1000);
+        s += calc(10000);
+        textView.setText(s);
+    }
+
+    private String calc(int n){
+        int a=0;
+        for(int i=0;i<n;i++)
+            a += i;
+        String s = "1から"+String.valueOf(n)+"を足した値は"+String.valueOf(a)+"です。\n";
+        return s;
     }
 }
